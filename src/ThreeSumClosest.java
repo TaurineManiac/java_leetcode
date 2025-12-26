6fb5264c8de8a293274b0bf9c04494a33fafddca
TaurineManiac <ignatladno@gmail.com>
2025-12-26 19:43:48 +0300

    find sol of 3Sum Closest, not easy, but it is btw 2 pinters and some math, find this sol in solutions, today try to repeat this sol

--- src/ThreeSumClosest.java
+++ src/ThreeSumClosest.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0] + nums[1] + nums[2];
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+    
+                if(Math.abs(sum-target) < Math.abs(result-target)){
+                    result = sum;
+                }
+
+                if(sum == target){
+                    return target;
+                }
+                else if(sum > target){
+                    k--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
+class SolThreeSumClosest {
+    public static void main(String[] args) {
+        ThreeSumClosest threeSumClosest = new ThreeSumClosest();
+        int[] nums = {0,0,0};
+        System.out.println(threeSumClosest.threeSumClosest(nums,1));
+    }
 }
