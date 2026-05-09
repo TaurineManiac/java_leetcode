b182a42d5eb6a7a4d89c8bad0748ad41a13e07f4
TaurineManiac <ignatladno@gmail.com>
2026-05-09 21:53:27 +0300

    add for continue streak

---
+++ src/CopyListWithRandomPointer.java
@@ -0,0 +1,16 @@
+class Node extends ListNode{
+    Node random;
+
+    public Node(int val){
+        super(val);
+        this.random = null;
+    }
+}
+
+public class CopyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+
+    }
+}
+
+
