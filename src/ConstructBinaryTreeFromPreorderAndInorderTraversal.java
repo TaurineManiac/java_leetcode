f3a103704d6855f6cb192e702f7755fd61beae39
TaurineManiac <ignatladno@gmail.com>
2026-05-25 21:24:59 +0300

    try to solve ths medium problem...

---
+++ src/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,17 @@
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+    public TreeNode recoursiveBuildTree(int[] preorder, int[] inorder, int StopPos) {
+        if(preorder.length == 0 || inorder.length == 0){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[0]);
+        for()
+
+    }
+}
+
+
