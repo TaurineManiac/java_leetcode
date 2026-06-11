367c671f7b4e64a9e271df124115863971299b04
TaurineManiac <ignatladno@gmail.com>
2026-06-11 10:04:36 +0300

    solve task from expert soft

--- src/MergeTwoSortedArrayListsExpertSoft.java
+++ src/MergeTwoSortedArrayListsExpertSoft.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class MergeTwoSortedArrayListsExpertSoft {
+    public void merge(ArrayList<Integer> a, ArrayList<Integer> b) {
+        int sizeOfFirstArrayList = a.size();
+        int sizeOfSecondArrayList = b.size();
+        for (int z = 0; z < sizeOfSecondArrayList; z++) {
+            a.add(0);
+        }
+        int i=sizeOfFirstArrayList-1;
+        int j=sizeOfSecondArrayList-1;
+        int newIndexOfFirstArrayList = sizeOfFirstArrayList + sizeOfSecondArrayList-1;
+        while(i>=0 && j>=0){
+            if(a.get(i)<=b.get(j)){
+                a.set(newIndexOfFirstArrayList--, b.get(j--));
+            }
+            else{
+                a.set(newIndexOfFirstArrayList--, a.get(i--));
+            }
+
+        }
+        while(j>=0){
+            a.set(newIndexOfFirstArrayList--, b.get(j--));
+        }
+
+    }
 }
